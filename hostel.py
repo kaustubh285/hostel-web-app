68b01a2ddb73ff2a57e6a892f6150b35f7589693
kaustubh285 <kaustubh285@gmail.com>
2019-10-19 17:36:06 +0530

    session timeout and file upload restricted to pdf only

--- hostel.py
+++ hostel.py
@@ -1,9 +1,30 @@
+# IMPORT
 from flask import Flask, render_template , request , redirect, url_for, session, flash
 from functools import wraps
+from datetime import timedelta
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
 
+
+#Application config
 app=Flask(__name__)
-
 app.secret_key = "BATMAN"
+app.permanent_session_lifetime = timedelta(minutes=20)
+app.config['uploaded_files'] = "C:\\Users\\kaustubh\\Documents\\Dr.Who\\AdminLTE-2.4.18 (2)\\uploaded"
+app.config['allowed_ext'] = "PDF"
+app.config['SQLALCHEMY_DATABASE_URL']
+
+
+#Other Functions
+def checkext(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".",1)[1]
+    print(ext)
+    if ext.upper() == app.config['allowed_ext']:
+        return True
+    else:
+        return False
 
 def login_required(f):
     @wraps(f)
@@ -17,6 +38,8 @@ def login_required(f):
 
 
 
+
+#Main Pages Of Website
 @app.route('/', methods=['GET','POST'])
 def login():
     error = None
@@ -25,8 +48,30 @@ def login():
             error = " Invalid Credentials! Please Try again"
         else:
             session['logged_in']=True
+            flash("Session Expired!!")
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+
+
+@app.route('/registration', methods=['GET','POST'])
+@login_required
+def registration():
+    error=""
+    if request.method=="POST":
+        uploaded_file =  request.files["fileupload"]
+        if uploaded_file.filename== "":
+            flash("Please select a file")
+        else:
+            print(uploaded_file.filename)
+            if not checkext(uploaded_file.filename):
+                flash("Please upload only PDF file")
+                #return render_template('registration.html', error= error)
+            else:
+                uploaded_file.save(os.path.join(app.config['uploaded_files'], "myname"))
+                return redirect(url_for('home'))
+    return render_template('registration.html')
+
 
 
 @app.route('/logout')
@@ -36,15 +81,11 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
+
 @app.route('/home')
 @login_required
 def home():
     return render_template('home.html')
-
-@app.route('/registration')
-@login_required
-def registration():
-    return render_template('registration.html')
 
 @app.route('/view-hostel')
 @login_required
